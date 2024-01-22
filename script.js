9712e88cb2f76292b4cd25afb4bdcd20e674b053
Zamira Estrada <156048180+Zamirae21@users.noreply.github.com>
2024-01-22 14:01:37 -0500

    Add files via upload
    Initial Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+var firstName = "Zamira";
+var lastName = "Estrada";
+var age = 20;
+
+alert("My name is " + firstName + " " + lastName);
+alert("I am " + age + " years old");
+console.log("Having so much fun in the class!");
+
