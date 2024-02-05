4fe1e7e87c969a20a17e3e57766a67d39c356bc2
Zamira Estrada <156048180+Zamirae21@users.noreply.github.com>
2024-02-05 13:33:46 -0500

    Commiting files on here, terminal was unsuccessful
     "Complete JavaScript assignment"

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// script.js
 var firstName = "Zamira";
 var lastName = "Estrada";
 var age = 20;
@@ -6,4 +5,28 @@ var age = 20;
 alert("My name is " + firstName + " " + lastName);
 alert("I am " + age + " years old");
 console.log("Having so much fun in the class!");
+
+// Logging even numbers from 1000 to 0
+for (let i = 1000; i >= 0; i -= 2) {
+  console.log(i);
+}
+
+// Alerts during the loop at specific points
+for (let i = 0; i <= 10000; i++) {
+  if (i === 2500) {
+    alert("A quarter of the way there!");
+  } else if (i === 5000) {
+    alert("Halfway there!");
+  } else if (i === 10000) {
+    alert("The loop is done!");
+  }
+}
+
+// Array of top 5 favorite TV shows and logging messages
+var topTvShows = ["Buccaneers", "Griselda", "The Vampire Diaries", "Emily in Paris", "Don't Look Up"];
+
+for (let i = 0; i < topTvShows.length; i++) {
+  console.log("My #" + (i + 1) + " favorite TV show is " + topTvShows[i]);
+}
+
 
